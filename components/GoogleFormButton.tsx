187ab9bfe374f1069915c13af0c5ba44e846d2db
Stoyan Germanov <s.p.germanov@gmail.com>
2025-05-14 20:47:34 +0300

    fix style, add Gospoda drugari

--- components/GoogleFormButton.tsx
+++ components/GoogleFormButton.tsx
@@ -1,12 +1,9 @@
 const GoogleFormButton = ({ formUrl }) => {
   return (
-    <a
-      href={formUrl}
-      target="_blank"
-      rel="noopener noreferrer"
-      className="hover:text-primary-500 dark:hover:text-primary-400 fixed top-7 right-4 z-80 h-16 w-16 p-4 text-gray-900 dark:text-gray-100"
-    >
-      <button>Попълни формуляра за поръчка</button>
+    <a href={formUrl} target="_blank" rel="noopener noreferrer">
+      <button className="bg-primary-500 hover:bg-primary-700 focus:ring-primary-600 dark:hover:bg-primary-400 w-auto rounded-md px-8 py-2 font-medium text-white focus:ring-2 focus:ring-offset-2 focus:outline-none sm:py-0 dark:ring-offset-black">
+        Попълни формуляра за поръчка
+      </button>
     </a>
   )
 }
