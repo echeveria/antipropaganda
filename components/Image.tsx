92e3fa926dc946f3d0f4b5e08a754c41557bf29b
Stoyan Germanov <s.p.germanov@gmail.com>
2025-05-27 18:28:29 +0300

    fix image title and alt

--- components/Image.tsx
+++ components/Image.tsx
@@ -2,8 +2,16 @@ import NextImage, { ImageProps } from 'next/image'
 
 const basePath = process.env.BASE_PATH
 
-const Image = ({ src, ...rest }: ImageProps) => (
-  <NextImage src={`${basePath || ''}${src}`} {...rest} />
+const Image = ({ src, alt, ...rest }: ImageProps) => (
+  <div style={{ textAlign: 'center', margin: '1rem 0' }}>
+    <NextImage
+      style={{ width: '100%', height: 'auto', marginBottom: '1rem' }}
+      src={`${basePath || ''}${src}`}
+      alt={`${alt || ''}`}
+      {...rest}
+    />
+    {alt && <p style={{ fontStyle: 'italic', fontSize: '1.3rem', margin: '0.1rem 0' }}>{alt}</p>}
+  </div>
 )
 
 export default Image
