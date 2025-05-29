117c1166b672cd83d12bdc0de131a4fd2fa9f712
Stoyan Germanov <s.p.germanov@gmail.com>
2025-05-29 22:45:15 +0300

    remove log

--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -47,7 +47,7 @@ export default async function TagPage(props: { params: Promise<{ tag: string }> 
     currentPage: 1,
     totalPages: totalPages,
   }
-  console.log([initialDisplayPosts, filteredPosts])
+
   return (
     <ListLayout
       posts={filteredPosts}
