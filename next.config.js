bf5abe9c6276a62a249fe1ad77e4b2dee2891321
Stoyan Germanov <s.p.germanov@gmail.com>
2025-05-30 18:57:33 +0300

    fix youtube iframe

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const ContentSecurityPolicy = `
   media-src *.s3.amazonaws.com;
   connect-src *;
   font-src 'self';
-  frame-src giscus.app
+  frame-src https://giscus.app https://www.youtube.com https://www.youtube-nocookie.com;
 `
 
 const securityHeaders = [
